3873f9514d898f91d41701f9c52989940ea6e428
Hao Nguyen <haodev910@gmail.com>
2023-08-28 13:21:32 +0700

    1st commit - khung login, dashboard

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+
+  templateUrl: './login.component.html',
+})
+export class LoginComponent implements OnInit{
+
+  ngOnInit(){
+
+  }
+}
