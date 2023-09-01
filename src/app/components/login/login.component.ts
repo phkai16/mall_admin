b2e28b15a185bffd8c0c8c2a797c68ba5e5534b4
Hao Nguyen <haodev910@gmail.com>
2023-09-01 12:40:24 +0700

    changes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountAPI } from 'src/app/models/accountapi.model';
+import { AccountAPIService } from 'src/app/services/accountapi.service';
 
 @Component({
 
@@ -6,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit{
 
+  username: string;
+  password: string;
+  account: AccountAPI;
+
+  constructor(
+    private router : Router,
+    private accountAPIService : AccountAPIService
+  ){}
+
   ngOnInit(){
+    
+  }
+
+  login(){
 
   }
+  
 }
---
+++ src/app/models/accountapi.model.ts
@@ -0,0 +1,10 @@
+export class AccountAPI {
+    id: number;
+    username: string;
+    password: string;
+    name: string;
+    gender: string;
+    status: boolean;
+    dob: Date;
+    created: Date
+}
